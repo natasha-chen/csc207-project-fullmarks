1e95404cf48a1b2fdd0f3a0ed1b616739ceca511
natasha-chen <natashachen44@gmail.com>
2025-11-11 14:34:40 -0500

    added interface adapter files for url view

---
+++ src/main/java/interface_adapter/url/URLState.java
@@ -0,0 +1,7 @@
+package interface_adapter.url;
+
+/**
+ * The state for the URL View Model.
+ */
+public class URLState {
+}
--- src/main/java/view/URLView.java
+++ src/main/java/view/URLView.java
@@ -1,4 +1,26 @@
 package view;
 
-public class URLView {
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+/**
+ * The View for when the user pastes the URL of choice.
+ */
+public class URLView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    JTextField textField = new JTextField(30); // 30 columns wide
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
