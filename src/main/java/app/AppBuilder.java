3a052c1f84852461030b874ce6ba25fab3a81495
ro208 <thejaws2015@yahoo.com>
2025-11-11 12:09:23 -0500

    test commit to export CA structure

---
+++ src/main/java/app/AppBuilder.java
@@ -0,0 +1,19 @@
+package app;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AppBuilder {
+    private final JPanel cardPanel = new JPanel();
+    private final CardLayout cardLayout = new CardLayout();
+    //TODO: figure out Entity Factory
+    final viewManagerModel viewManagerModel = new ViewManagerModel();
+    ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
+
+    private DownloadView downloadView;
+    private ConvertView convertView;
+    private SavetoView savetoView;
+    private ProgressView progressView;
+    private ExcludeView excludeView;
+
+}
---
+++ src/main/java/use_case/select_for_conversion/SelectForConversionInputData.java
@@ -0,0 +1,13 @@
+package use_case.select_for_conversion;
+import java.util.ArrayList;
+
+/**
+ * The input data for the Select for Conversion Use Case.
+ */
+public class SelectForConversionInputData {
+
+    private final ArrayList<String> playlist;
+    public SelectForConversionInputData(ArrayList<String> playlist) {
+        this.playlist = playlist;
+    }
+}
