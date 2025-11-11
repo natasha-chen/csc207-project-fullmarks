95ad0e6d347d37f352396a43749ab9f07af05f25
natasha-chen <natashachen44@gmail.com>
2025-11-11 13:41:26 -0500

    added view files

---
+++ src/main/java/view/ViewManager.java
@@ -0,0 +1,15 @@
+package view;
+
+import interface_adapter.ViewManagerModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * The View Manager for the program. It listens for property change events
+ * in the ViewManagerModel and updates which View should be visible.
+ */
+public class ViewManager {
+}
