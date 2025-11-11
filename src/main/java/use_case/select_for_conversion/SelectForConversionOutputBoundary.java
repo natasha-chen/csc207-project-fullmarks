96fd013e80a4589cfbdafc1cf4969426657c32a4
ro208 <thejaws2015@yahoo.com>
2025-11-11 12:09:23 -0500

    test commit to export CA structure

---
+++ src/main/java/use_case/select_for_conversion/SelectForConversionOutputBoundary.java
@@ -0,0 +1,14 @@
+package use_case.select_for_conversion;
+
+/**
+ * The input boundary for the Select for Conversion Use Case.
+ */
+public interface SelectForConversionOutputBoundary {
+
+    /**
+     * Execute the Select for Conversion Use Case.
+     * @param outputData the output data for this use case
+     */
+    void execute(SelectForConversionOutputData outputData);
+
+}
